5e5e18cc00c8a9c2ae9d7e2bda887cff4bfa3b97
josieldev0 <josiel.lira01@gmail.com>
2025-08-19 11:44:14 -0300

    Update aulas 27 e 29

--- 27_funcoes_def.py
+++ 27_funcoes_def.py
@@ -20,9 +20,30 @@
 # imprimir(1, 2, 3)
 # imprimir(4, 5, 6)
 
-def saudacao(nome = 'Sem nome'):
-    print(f'Olá, {nome}!')
+# def saudacao(nome = 'Sem nome'):
+#     print(f'Olá, {nome}!')
 
-saudacao('Josiel')
-saudacao('Ana')
-saudacao()
+# saudacao('Josiel')
+# saudacao('Ana')
+# saudacao()
+
+"""
+Valores padrão para parâmetros
+Ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será
+usado.
+Refatorar: editar o seu código.
+"""
+
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+soma(1,2)
+soma(3,5)
+soma(100,200)
+soma(100,200, 0)
+soma(x=7, z=9, y=0) # fora de ordem
