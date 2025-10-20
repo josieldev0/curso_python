5e96021f44cc57e0cc17d4324652bb7288d634ca
josieldev0 <josiel.lira01@gmail.com>
2025-10-20 14:36:48 -0300

    Create aula 30

---
+++ 30_return.py
@@ -0,0 +1,18 @@
+"""
+Retorno de valores das  funções (return)
+"""
+def soma(x, y):
+    if x > 10:
+        return [10, 20, 30] #retorna oq quiser []=list
+    return x + y
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+
+print(soma1)
+print(soma2)
+print(soma1 + soma2)
+print(soma(11, 2))
+
+
+
