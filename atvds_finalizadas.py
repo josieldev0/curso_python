161fe3bc9219e5aec55f749d2939bcf745df239c
josieldev0 <josiel.lira01@gmail.com>
2025-10-21 19:23:56 -0300

    Update atvds_finalizadas

--- atvds_finalizadas.py
+++ atvds_finalizadas.py
@@ -252,3 +252,23 @@ erros de índices inexistentes na lista.
     
 #-------------------------------------------------------------------------------
 
+"""
+Crie funções que duplicam, triplicam, e quadruplicam
+o número recebido como parâmetro
+"""
+
+# def criar_multiplicar(multiplicador):
+#     def multiplicar(numero):
+#         return numero * multiplicador
+#     return multiplicar
+
+# duplicar = criar_multiplicar(2)
+# triplicar = criar_multiplicar(3)
+# quadruplicar = criar_multiplicar(4)
+
+# print(duplicar(2))
+# print(triplicar(2))
+# print(quadruplicar(2))
+
+#-------------------------------------------------------------------------------
+
