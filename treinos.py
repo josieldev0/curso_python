351e5760ef8b4630e657a31c41b0b908d2930207
josieldev0 <josiel.lira01@gmail.com>
2025-11-03 16:51:27 -0300

    Update treinos

--- treinos.py
+++ treinos.py
@@ -446,3 +446,20 @@
 
 #-------------------------------------------------------------------------------
 
+# from datetime import date
+# data_atual = date.today().year
+# maior = 0
+# menor = 0
+
+# for c in range(1, 8):
+#     nasc = int(input(f'Em que ano a {c}ª pessoa nasceu? '))
+#     idade = data_atual - nasc
+#     if idade >= 21:
+#         maior += 1
+#     else:
+#         menor += 1
+
+# print(f'Ao todo tivemos {maior} pessoas maiores de idade ')
+# print(f'E também tivemos {menor} pessoas menore de idade')
+
+#-------------------------------------------------------------------------------
