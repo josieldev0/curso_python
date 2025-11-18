27b308ecbd02f5f9161037725ff47f185cd7c5e3
josieldev0 <josiel.lira01@gmail.com>
2025-11-18 14:46:20 -0300

    Update treinos
    treinando listas []

--- treinos.py
+++ treinos.py
@@ -964,3 +964,31 @@
 
 #-------------------------------------------------------------------------------
 
+# numeros = []
+# maior = 0
+# menor = 0
+
+# for posicao in range(0, 5):
+#     numeros.append(int(input(f'Digite um valor para a posição {posicao}: ')))
+
+#     if posicao == 0:
+#         maior = menor = numeros[posicao]
+#     else:
+#         if numeros[posicao] > maior:
+#             maior = numeros[posicao]
+#         if numeros[posicao] < menor:
+#             menor = numeros[posicao]
+
+# print(f'Você digitou os valores {numeros}')
+
+# print(f'O maior valor foi {maior} nas posições ', end='')
+# for indice, valor in enumerate(numeros):
+#     if valor == maior:
+#         print(f'{indice}')
+
+# print(f'O menor valor foi {menor} nas posições ', end='')
+# for indice, valor in enumerate(numeros):
+#     if valor == menor:
+#         print(f'{indice}')
+
+#-------------------------------------------------------------------------------
