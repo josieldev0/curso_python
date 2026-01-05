ffdc62d3b2de66f3b34bfde63e0ec85d181c88cc
josieldev0 <josiel.lira01@gmail.com>
2026-01-05 14:04:29 -0300

    Update treinos
    treinando funções
    def
    return

--- treinos.py
+++ treinos.py
@@ -1585,3 +1585,37 @@
 
 #-------------------------------------------------------------------------------
 
+# from datetime import date
+# ano = date.today().year
+
+# def voto(nasc):
+#     idade = ano - nasc
+#     if idade < 16:
+#         return f'Com {idade} anos: Não vota'
+#     elif 16 <= idade < 18 or idade > 65:
+#         return f'Com {idade} anos: Voto Opcional'
+#     else: 
+#         return f'Com {idade} anos: Voto Obrigatório'
+
+
+# nasc = int(input('Em que ano você nasceu?'))
+# print(voto(nasc))
+
+#-------------------------------------------------------------------------------
+
+# def fatorial(n, show=False):
+#     f = 1
+#     for c in range(n, 0, -1):
+#         if show:
+#             print(c, end='')
+#             if c > 1: 
+#                 print(' x ', end='')
+#             else:
+#                 print(' = ', end='')
+#         f += c
+#     return f
+    
+# print(fatorial(5, show=True))
+
+#-------------------------------------------------------------------------------
+
